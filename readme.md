95c5a36dda23e318180e6438dd089b6790be8de0
Lucas Santana do Nascimento Portela <contact@lucasportela.dev>
2022-03-02 22:22:01 -0300

    let there be code!

---
+++ readme.md
@@ -0,0 +1,3 @@
+## Clean Typescript/SASS Webpack Boilerplate
+
+A **simple** and **clean** start point for **awesome** and **complex** projects brought to you by [Lucas Portela](https://lucasportela.dev). Use and share it freely!
---
+++ webpack.common.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  performance:{
+    hints: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: 'src/index.html'
+  })],
+};
